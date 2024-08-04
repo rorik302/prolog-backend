aae08a65a4335c3f22d91dc1312b7c310cdca936
Aleksandr Danilenko <rorik302@gmail.com>
2024-08-04 21:45:51 +0300

    feat feat: настроены миграции моделей тенантов

--- src/prolog_backend/config/alembic.py
+++ src/prolog_backend/config/alembic.py
@@ -8,6 +8,7 @@ from prolog_backend.config.directories import PROJECT_DIR
 class AlembicSettings(BaseSettings):
     INI_PATH: Path = PROJECT_DIR / "alembic.ini"
     SHARED_INI_SECTION: str = "shared"
+    TENANT_INI_SECTION: str = "tenant"
 
 
 alembic_settings: AlembicSettings = AlembicSettings()
--- src/prolog_backend/migrations/tenant/env.py
+++ src/prolog_backend/migrations/tenant/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from prolog_backend.config.database import database_settings
 from prolog_backend.migrations.utils import alembic_include_object, alembic_process_revision_directives
-from prolog_backend.models import SharedModel
+from prolog_backend.models import TenantModel
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
@@ -12,30 +12,30 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = SharedModel.metadata
+target_metadata = TenantModel.metadata
 
 config.set_main_option("sqlalchemy.url", database_settings.DB_URL)
 
 
 def run_migrations_online() -> None:
+    schema_name = config.attributes.get("schema_name")
+    assert schema_name, "Нужно передать имя схемы для миграции"
     session: Session = config.attributes.get("session")
-    assert session, "Нужно передать сессию для миграции"
 
     connection = session.connection()
-    connection.execute(text(f"SET search_path TO {database_settings.SHARED_SCHEMA_NAME}"))
+    connection.execute(text(f"SET search_path TO {schema_name}"))
     connection.commit()
-    connection.dialect.default_schema_name = database_settings.SHARED_SCHEMA_NAME
+    connection.dialect.default_schema_name = schema_name
+
     context.configure(
-        connection=session.connection(),
+        connection=connection,
         target_metadata=target_metadata,
         include_object=alembic_include_object(),
-        process_revision_directives=alembic_process_revision_directives(config=config, context=context),
+        process_revision_directives=alembic_process_revision_directives(config, context),
     )
 
     with context.begin_transaction():
         context.run_migrations()
-
-    connection.close()
 
 
 run_migrations_online()
