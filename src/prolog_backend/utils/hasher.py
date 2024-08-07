cb424b3853356204de374d3fb05f47bfc48384af
Aleksandr Danilenko <rorik302@gmail.com>
2024-08-07 23:43:00 +0300

    fix: обновлен Hasher

--- src/prolog_backend/utils/hasher.py
+++ src/prolog_backend/utils/hasher.py
@@ -1,7 +1,22 @@
+import hashlib
+from enum import Enum
+
 from argon2 import PasswordHasher
+
+
+class Algorithm(str, Enum):
+    ARGON2 = "ARGON2"
+    SHA1 = "SHA1"
 
 
 class Hasher:
     @classmethod
-    def hash(cls, value: str) -> str:
-        return PasswordHasher().hash(value)
+    def hash(cls, value: str, algorithm: Algorithm = Algorithm.ARGON2) -> str:
+        if algorithm == Algorithm.ARGON2:
+            return PasswordHasher().hash(value)
+        if algorithm == Algorithm.SHA1:
+            return hashlib.sha1(string=value.encode("utf-8")).hexdigest()
+
+    @classmethod
+    def verify(cls, hashed: str, password: str) -> bool:
+        return PasswordHasher().verify(hash=hashed, password=password)
